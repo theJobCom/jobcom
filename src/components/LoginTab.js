4479877387ffff4f0a2a0e973be8e47f51d3deb1
daudi13 <davouma13@gmail.com>
2023-01-17 15:21:06 +0300

    Install useForm

--- src/components/LoginTab.js
+++ src/components/LoginTab.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import { makeStyles } from 'tss-react/mui';
+import { TextField, Button } from '@mui/material';
+import { Box } from '@mui/system';
+import { useform } from 'react-hook-form'
+
 
 const LoginTab = () => {
   return (
-    <div>LoginTab</div>
+    <Box p={3}>
+      <TextField
+      variant='outlined'
+      type="email"
+      label="username"
+      fullWidth
+      />
+      <TextField
+      variant='outlined'
+      type="email"
+      label="email"
+      fullWidth
+      />
+      <TextField
+      variant='outlined'
+      type="email"
+      label="password"
+      fullWidth
+      />
+      <Button
+        variant="contained"
+        type="submit"
+        size="large"
+      >
+      </Button>
+    </Box>
   )
 }
 
