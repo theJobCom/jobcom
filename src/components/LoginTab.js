ca761a64061277054278cf9f5ca3a235093e7078
daudi13 <davouma13@gmail.com>
2023-01-17 15:51:00 +0300

    Add Login tab form

--- src/components/LoginTab.js
+++ src/components/LoginTab.js
@@ -1,38 +1,53 @@
 import React from 'react'
 import { makeStyles } from 'tss-react/mui';
 import { TextField, Button } from '@mui/material';
-import { Box } from '@mui/system';
-import { useform } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 
 
 const LoginTab = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = async (data) => {
+    console.log(data)
+  }
+
   return (
-    <Box p={3}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <TextField
       variant='outlined'
-      type="email"
+      type="text"
       label="username"
       fullWidth
+        {...register("username", { required: "Add Your Username" })}
+        error={!!errors?.username}
+        helperText={errors?.username ? errors.username.message : null} 
       />
       <TextField
       variant='outlined'
       type="email"
       label="email"
       fullWidth
+      {...register("email", { required: "Add Your Email" })}
+        error={!!errors?.email}
+        helperText={errors?.email ? errors.email.message : null}
       />
       <TextField
       variant='outlined'
-      type="email"
+      type="password"
       label="password"
       fullWidth
+      {...register("password", { required: "Add Your password" })}
+      error={!!errors?.password}
+      helperText={errors?.password ? errors.password.message : null}
       />
       <Button
         variant="contained"
         type="submit"
         size="large"
       >
+        Login
       </Button>
-    </Box>
+    </form>
   )
 }
 
