3d81b46b7e7f59cce83f5e755298e23e57b120d1
daudi13 <davouma13@gmail.com>
2023-01-17 15:51:39 +0300

    Add signUp tab form

--- src/components/SignUpTab.js
+++ src/components/SignUpTab.js
@@ -1,8 +1,57 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from 'tss-react/mui';
+import { TextField, Button } from '@mui/material';
+import { useForm } from 'react-hook-form'
 
 const SignUpTab = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  
   return (
-    <div>SignUpTab</div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <TextField
+      variant='outlined'
+      type="text"
+      label="username"
+      fullWidth
+        {...register("username", { required: "Add Your Username" })}
+        error={!!errors?.username}
+        helperText={errors?.username ? errors.username.message : null} 
+      />
+      <TextField
+      variant='outlined'
+      type="email"
+      label="email"
+      fullWidth
+      {...register("email", { required: "Add Your Email" })}
+        error={!!errors?.email}
+        helperText={errors?.email ? errors.email.message : null}
+      />
+      <TextField
+      variant='outlined'
+      type="password"
+      label="password"
+      fullWidth
+      {...register("password", { required: "Add Your password" })}
+      error={!!errors?.password}
+      helperText={errors?.password ? errors.password.message : null}
+      />
+      <TextField
+      variant='outlined'
+      type="password"
+      label="comfirm password"
+      fullWidth
+      {...register("confirmPassword", { required: "Add Your password" })}
+      error={!!errors?.confirmPassword}
+      helperText={errors?.confirmPassword ? errors.confirmPassword.message : null}
+      />
+      <Button
+        variant="contained"
+        type="submit"
+        size="large"
+      >
+        Login
+      </Button>
+    </form>
   )
 }
 
