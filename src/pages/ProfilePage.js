0f601c8564f136031e35b841c1c29b7f24289907
daudi13 <davouma13@gmail.com>
2023-01-20 19:00:20 +0300

    Add leftBox styling

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,52 +1,73 @@
 import { Typography, Button } from '@mui/material';
-import { Box } from '@mui/system';
+import { Box, letterSpacing } from '@mui/system';
 import React from 'react'
 import { makeStyles } from 'tss-react/mui'
-import { BsPlusLg } from 'react-icons/bs';
+import SideBar from '../components/SideBar';
+import CameraIcon from '../icons/Icons/camera.png'
 
 const ProfilePage = () => {
   const useStyle = makeStyles()(() => ({
-    container: { 
-      width: "100vw",
+    container: {
       height: "100vh",
       display: "flex",
-      alignItems: "center",
+    },
+    mainBox: {
+      padding: "60px",
+      display: "flex",
+      gap: "50px",
       justifyContent: "center"
     },
-    wrapper: {
+    leftBox: {
       display: "flex",
       flexDirection: "column",
-      gap: "30px"
+      gap: "24px"
     },
-    topbox: {
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      textAlign: "center",
-      gap: "30px"
+    imageBox: {
+      width: "260px",
+      height: "260px",
+      position: "relative",
+      borderRadius: "10px",
     },
-    meta: {
-      display: "flex",
-      gap: "20px",
-      justifyContent: "space-between"
+    imageHolder: {
+      width: "100%",
+      height: "100%",
+      backgroundColor: "#f2f4f7",
+      borderRadius: "10px"
     },
-    profileImage: {
-      width: "200px",
-      height: "200px",
+    cameraIconBox: {
+      position: "absolute",
+      width: "64px",
+      height: "64px",
+      bottom: "10px",
+      right: "10px",
       borderRadius: "50%",
-      backgroundColor: "#c0c0c0"
-    },
-    bottomBox: {
+      backgroundColor: "#e4e7ec",
       display: "flex",
-      flexDirection: "column",
       alignItems: "center",
       justifyContent: "center",
-      borderTop: "2px solid #c0c0c0",
-      gap: "30px",
-      borderBottom: "2px solid #c0c0c0",
-      width: "800px",
-      height: "300px",
       cursor: "pointer"
+    },
+    username: {
+      fontWeight: "600",
+      fontSize: "22px",
+      lineHeight: "28px",
+      color: "#344054"
+    },
+    userData: {
+      display: "flex",
+      alignItems: "center",
+      width: "150px",
+      justifyContent: "space-between"
+    },
+    data: {
+      fontWeight: "500",
+      fontSize: "16px",
+      lineHeight: "24px",
+      letterSpacing: "0.15px",
+      color: "#344054"
+    },
+    num: {
+      fontWeight: "700",
     }
   }))
 
@@ -54,19 +75,34 @@ const ProfilePage = () => {
 
   return (
     <div className={classes.container}>
-      <Box className={classes.wrapper}>
-        <Box className={classes.topbox}>
-          <Typography variant="h3">Name</Typography>
-          <Box className={classes.meta}>
-            <Typography variant="subtitle1">0 Likes</Typography>
-            <Typography variant="subtitle1">0 Visits</Typography>
+      <SideBar />
+      <Box className={classes.mainBox}>
+        <Box className={classes.leftBox}>
+          <Box className={classes.imageBox}>
+            <img className={classes.imageHolder} />
+            <Box className={classes.cameraIconBox}>
+              <img src={CameraIcon} alt="camera icon"/>
+            </Box>
           </Box>
-          <img src=" " className={classes.profileImage} />
-          <Button variant="outlined">Upload Profile Picture</Button>
+          <Box className={classes.metaData}>
+            <p className={classes.username}>@john.doe</p>
+            <Box className={classes.userData}>
+              <p className={classes.data}><span className={classes.num}>0</span> Likes</p>
+              <p className={classes.data}><span className={classes.num}>0</span> Visits</p>
+            </Box>
+          </Box>
         </Box>
-        <Box className={classes.bottomBox}>
-          <BsPlusLg style={{fontSize: "2rem"}} />
-          <Typography variant="subtitle1">Tailor Your Portfolio</Typography>
+        <Box className={classes.rightBox}>
+          <Box className={classes.intro}>
+          <h2 className={classes.title}>Welcome to JobCom</h2>
+          <h3 className={classes.tagline}>One-Click to showcase your best work in one site</h3>
+          </Box>
+          <ol className={classes.list}>
+            <li>Add Basic information about you</li>
+            <li>Add more stuff, like projects or achievements</li>
+            <li>Add an image and hightlight your favorite project</li>
+          </ol>
+          <Button>+ Make your Own Portfolio</Button>
         </Box>
       </Box>
     </div>
