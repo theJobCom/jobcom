5ffa2b7925e1fbd8f826b56be17be33083966e92
daudi13 <davouma13@gmail.com>
2023-02-03 12:12:54 +0300

    Add date converter function

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -277,6 +277,54 @@ const ProfilePage = () => {
 
   const { classes } = useStyle();
 
+  const dateFunc = (str) => {
+    const dateArr = (str.split('-').slice(0, 2))
+    let month;
+    switch (+dateArr[1]) {
+      case 1:
+        month = "January"
+        break;
+      case 2:
+        month = "February"
+        break;
+      case 3:
+        month = "March"
+        break;
+      case 4:
+        month = "April"
+        break;
+      case 5:
+        month = "May"
+        break;
+      case 6:
+        month = "June"
+        break;
+      case 7:
+        month = "July"
+        break;
+      case 8:
+        month = "August"
+        break;
+      case 9:
+        month = "September"
+        break;
+      case 10:
+        month = "October"
+        break;
+      case 11:
+        month = "November"
+        break;
+      case 12:
+        month = "December"
+        break;
+      default:
+        month = ''
+    }
+    console.log(`${month} ${dateArr[0]}`)
+  }
+
+  dateFunc('2021-11-18');
+
 
   return (
     <div className={classes.container}>
