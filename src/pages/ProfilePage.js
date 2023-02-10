154de78b3ef8987ada65613d53a6c8470a9c05c2
daudi13 <davouma13@gmail.com>
2023-02-10 08:17:31 +0300

    Add contact icon if only user data exist

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -363,8 +363,8 @@ const ProfilePage = () => {
               {
                 contactInfo &&
               <Box className={classes.contactBox}>
-                {contactInfo?.linkedIn === "" ? "" : <Link href={contactInfo?.linkedIn} className={classes.linkedIn}><GrLinkedin/></Link>}
                 {contactInfo?.role === "" ? "" :<Link href={contactInfo?.role} className={classes.gthb}><GoMarkGithub/></Link>}
+                {contactInfo?.linkedIn === "" ? "" : <Link href={contactInfo?.linkedIn} className={classes.linkedIn}><GrLinkedin/></Link>}
                 {contactInfo?.behance === "" ? "" : <Link href={contactInfo?.behance} className={classes.medium}><ImBehance2/></Link>}
                 <Link href={contactInfo?.email} className={classes.medium}><MdAlternateEmail/></Link>
                 {contactInfo?.portfolio === "" ? "" :<Link href={contactInfo?.portfolio} className={classes.medium}><BsLaptop/></Link>}
