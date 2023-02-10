9aef4b05fef997088b5ca54eedea1613be46571d
daudi13 <davouma13@gmail.com>
2023-02-10 12:30:09 +0300

    Add HandleYears function to return years arrays

--- src/utils/years.js
+++ src/utils/years.js
@@ -1,3 +1,14 @@
-const year = (new Date()).getFullYear();
+export const handleYears = (arr) =>{
+  const year = (new Date()).getFullYear();
+  let projectYears = []
+  for (let i = 0; i < arr.length; i++) {
+    let years = year - i
+    projectYears.push(years)
+  }
+  return projectYears
+}
 
-console.log(year)
+
+
+const prevYears = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35];
+handleYears(prevYears);
