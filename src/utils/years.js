ce3f452f86a327104e584b1b0bd23f2c7ebe4bec
daudi13 <davouma13@gmail.com>
2023-02-10 12:44:19 +0300

    Add year dynamically

--- src/utils/years.js
+++ src/utils/years.js
@@ -7,8 +7,3 @@ export const handleYears = (arr) =>{
   }
   return projectYears
 }
-
-
-
-const prevYears = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35];
-handleYears(prevYears);
