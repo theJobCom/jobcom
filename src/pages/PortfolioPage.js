cc9e695c688b98383b8d263a379c43b3ed1fa0de
daudi13 <davouma13@gmail.com>
2023-02-15 14:04:55 +0300

    Add Avatar

--- src/pages/PortfolioPage.js
+++ src/pages/PortfolioPage.js
@@ -118,6 +118,18 @@ const PortfolioPage = () => {
     return () => unsubScribe()
   }, [])
 
+  useEffect(() => {
+    const q = query(collection(db, "Avatars"), where("createdBy", "==", doc(db, "User", id)));
+    const unsubScribe = onSnapshot(q, (snapShot) => {
+      let dataArr = []
+      snapShot.docs.forEach((doc) => {
+        dataArr.push({ ...doc.data(), id: doc.id })
+      })
+      setAvatar(dataArr)
+    })
+    return () => unsubScribe()
+  }, []);
+
   const generalInfo = general[0];
   const educationInfo = education;
   const contactInfo = contact[0];
