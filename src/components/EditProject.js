6dc70214ee2bb3e82df16576f8b13b154971a026
daudi13 <davouma13@gmail.com>
2023-02-22 08:59:35 +0300

    Add project modal

--- src/components/EditProject.js
+++ src/components/EditProject.js
@@ -1,29 +1,17 @@
 import React from 'react'
 import { makeStyles } from 'tss-react/mui';
-import { FormControl, InputLabel, TextField, Select, MenuItem, Button } from '@mui/material';
+import { TextField, Button } from '@mui/material';
 import { Textarea } from '@mui/joy';
 import { useForm } from 'react-hook-form';
 import { doc, updateDoc } from 'firebase/firestore';
 import firebaseEngine from '../initFirebase/configureFirebase';
 import { MdCancel } from 'react-icons/md';
 import { DataStoreState } from '../store/ContexApi';
-import { handleYears } from '../utils/years';
 
 const EditProject = ({closeEditProject, experienceData}) => {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const { setAlert } = DataStoreState();
-  const [location, setLocation] = React.useState('');
-  const [year, setYear] = React.useState('');
   const { db } = firebaseEngine;
-    const prevYears = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35];
-
-  const handleChange = (event) => {
-    setLocation(event.target.value)
-  }
-
-  const handleChangeYear = (event) => {
-    setYear(event.target.value)
-  }
 
 
   const onSubmit = async (data) => {
@@ -97,49 +85,29 @@ const EditProject = ({closeEditProject, experienceData}) => {
         helperText={errors?.userprojectName ? errors.projectName.message : null}
       />
       <label className={classes.label}>Year*</label>
-      <FormControl className={classes.input}>
-        <InputLabel id="demo-simple-select-helper-label">Select year*</InputLabel>
-        <Select
-          labelId="demo-simple-select-helper-label"
-          id="demo-simple-select-helper"
-          value={year}
-          defaultValue={experienceData.year}
-          {...register("year", { required: "Please add project year" })}
-          error={!!errors?.year}
-          helperText={errors?.year ? errors.year.message : null}
-          label="Select Year"
-          onChange={handleChangeYear}
-        >
-          {
-            handleYears(prevYears).map((year) => {
-              return (
-                <MenuItem value={`${year}`}>{year}</MenuItem>
-              )
-            })
-          }
-        </Select>
-      </FormControl>
+      <TextField
+      className={classes.input}
+        variant='outlined'
+        type="number"
+        fullWidth
+        defaultValue={experienceData.year} 
+        {...register("year", { required: "Please add project year" })}
+        error={!!errors?.year}
+        helperText={errors?.year ? errors.year.message : null}
+        label="Select Year"
+      />
       <label className={classes.label}>Project Category*</label>
-      <FormControl className={classes.input}>
-        <InputLabel id="demo-simple-select-helper-label">Select Project category</InputLabel>
-        <Select
-          labelId="demo-simple-select-helper-label"
-          id="demo-simple-select-helper"
-          value={location}
-          defaultValue={experienceData.category}
-          {...register("category", { required: "Please add category" })}
-          error={!!errors?.category}
-          helperText={errors?.category ? errors.category.message : null}
-          label="Select Project category"
-          onChange={handleChange}
-        >
-          <MenuItem value={"Ai"}>Ai</MenuItem>
-          <MenuItem value={"FinTech"}>FinTech</MenuItem>
-          <MenuItem value={"E-commerce"}>E-commerce</MenuItem>
-          <MenuItem value={"Education"}>Education</MenuItem>
-          <MenuItem value={"Other"}>Other</MenuItem>
-        </Select>
-      </FormControl>
+      <TextField
+      className={classes.input}
+        variant='outlined'
+        type="text"
+        fullWidth
+        label="project name"
+        defaultValue={experienceData.category}
+        {...register("category", { required: "Please add category" })}
+        error={!!errors?.category}
+        helperText={errors?.category ? errors.category.message : null}
+      />
       <label className={classes.label}>Description</label>
       <Textarea
         className={classes.textarea}
